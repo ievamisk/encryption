61bdfe9bce8acf6f889b1f00ebd1470ada9460fa
ievamisk <ieva.misk@gmail.com>
2018-01-11 23:47:51 +0200

    Final project commit

---
+++ Encryption/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.encryptButton = new System.Windows.Forms.RadioButton();
+            this.decryptButton = new System.Windows.Forms.RadioButton();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // directoryBox
+            // 
+            this.directoryBox.Location = new System.Drawing.Point(107, 36);
+            this.directoryBox.Multiline = true;
+            this.directoryBox.Name = "directoryBox";
+            this.directoryBox.Size = new System.Drawing.Size(432, 27);
+            this.directoryBox.TabIndex = 0;
+            // 
+            // browseButton
+            // 
+            this.browseButton.AutoSize = true;
+            this.browseButton.Location = new System.Drawing.Point(545, 36);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(140, 27);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Directory";
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.AutoSize = true;
+            this.encryptButton.Location = new System.Drawing.Point(107, 82);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(77, 21);
+            this.encryptButton.TabIndex = 6;
+            this.encryptButton.TabStop = true;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.AutoSize = true;
+            this.decryptButton.Location = new System.Drawing.Point(211, 82);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(78, 21);
+            this.decryptButton.TabIndex = 7;
+            this.decryptButton.TabStop = true;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.AutoSize = true;
+            this.startButton.Location = new System.Drawing.Point(107, 153);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(189, 27);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.AutoSize = true;
+            this.pauseButton.Location = new System.Drawing.Point(301, 153);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(189, 27);
+            this.pauseButton.TabIndex = 9;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.AutoSize = true;
+            this.cancelButton.Location = new System.Drawing.Point(496, 153);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(189, 27);
+            this.cancelButton.TabIndex = 11;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(107, 115);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(578, 21);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 201);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(578, 80);
+            this.textBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 305);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.directoryBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox directoryBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton encryptButton;
+        private System.Windows.Forms.RadioButton decryptButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Encryption/Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace Encryption
+{
+    public partial class Form1 : Form
+    {
+        private string backupPath;
+        private string zipPath;
+        private string newDir;
+        private string selectedPath;
+        private int count;
+        StreamWriter sw;
+        public ManualResetEvent mre = new ManualResetEvent(true);
+
+        Thread runTask;
+
+        bool pause;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    directoryBox.Text = (fbd.SelectedPath).ToString();
+                    selectedPath = directoryBox.Text;
+                    backupPath = @"" + selectedPath + "-Copy";
+                    zipPath = @"" + selectedPath + "\\" + "Zipped.zip";
+                }
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            
+            pause = false;
+            if (encryptButton.Checked)
+            {
+                runTask = new Thread(() => EncryptFile(selectedPath));
+                runTask.Start();
+
+            }
+            else if (decryptButton.Checked)
+            {
+                runTask = new Thread(() => DecryptFile(selectedPath));
+                runTask.Start();
+            }
+        }
+
+        #region ENCRYPTION
+        private void moveItems(string startPath, string destinationPath)
+        {
+            try
+            {
+                Directory.CreateDirectory(backupPath);
+
+                foreach (var subdirectory in Directory.GetDirectories(startPath))
+                {
+                    string dirName = Path.GetFileName(subdirectory);
+                    newDir = String.Format(@"" + destinationPath + "\\" + dirName);
+                    Directory.Move(subdirectory, Path.Combine(subdirectory, newDir));   
+                }
+
+                ZipFile.CreateFromDirectory(backupPath, zipPath);
+
+                foreach (var file in Directory.GetFiles(startPath))
+                {
+                    string fileName = Path.GetFileName(file);
+                    string destFile = Path.Combine(destinationPath, fileName);
+                    File.Copy(file, destFile);
+                    count++;
+                }
+                this.BeginInvoke((MethodInvoker)delegate
+                {
+                    progressBar1.Value = 0;
+                    progressBar1.Maximum = count * 2;
+                    progressBar1.Minimum = 0;
+
+                });
+
+                foreach (var file in Directory.GetFiles(backupPath)) {
+                    if (file.Contains("Zipped.zip"))
+                    {
+                        File.Delete(file);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ooops! Problem with moving items :(");
+            }
+}
+
+
+        
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        public void EncryptFile(string path)
+        {
+            string password = "12345678";
+            moveItems(selectedPath, backupPath);
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                mre.WaitOne(Timeout.Infinite);
+                byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+                File.WriteAllBytes(file, bytesEncrypted);
+
+                this.BeginInvoke((MethodInvoker)delegate
+                {
+                    progressBar1.Value +=1;
+
+                });
+            }
+            CreateHash(selectedPath);
+            MessageBox.Show("Encryption successful!");
+
+        }
+
+        public string GetHash(string file)
+        {
+            var sBuilder = new StringBuilder();
+
+            using (var md5 = MD5.Create())
+            {
+                byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(file));
+
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    sBuilder.Append(hash[i].ToString("x2"));
+                }
+            }
+            return sBuilder.ToString();
+        }
+
+        public void CreateHash(string path)
+        {
+            try
+            {
+                sw = new StreamWriter(@"D:\Users\ievam\Desktop\hash.txt", true);
+
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    sw.WriteLine(GetHash(file));
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        progressBar1.Value += 1;
+
+                    });   
+                }
+                sw.Close();
+            }
+            catch (Exception)
+            {
+               MessageBox.Show("Oops! Something went wrong...");
+            }
+        }
+        #endregion
+
+        #region DECRYPTION
+        public byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        public void DecryptFile(string path)
+        {
+            string password = "12345678";
+            string[] hashes = File.ReadAllLines(@"D:\Users\ievam\Desktop\hash.txt");
+            int count = Directory.GetFiles(path).Length;
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                progressBar1.Value = 0;
+                progressBar1.Maximum = count;
+                progressBar1.Minimum = 0;
+            });
+
+            foreach (string file in Directory.GetFiles(path))
+            {
+                mre.WaitOne(Timeout.Infinite);
+
+                if (hashes.Contains(GetHash(file)))
+                {
+                    byte[] bytesToBeDecrypted = File.ReadAllBytes(file);
+                    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+                    passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                    byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+                    File.WriteAllBytes(file, bytesDecrypted);
+
+                    if (file.Contains("Zipped.zip")) {
+                        unzipSubfolders(zipPath, selectedPath);
+                    }
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        progressBar1.Value += 1;
+
+                    });
+                }
+                else
+                {
+                    MessageBox.Show("Oops, different hash!");
+                }
+            }
+            MessageBox.Show("Decryption successful!");
+
+            DirectoryInfo di = new DirectoryInfo(backupPath);
+
+           }
+
+        private void unzipSubfolders(string startPath, string destinationPath)
+        {
+            try
+            {
+                ZipFile.ExtractToDirectory(startPath, selectedPath);
+                File.Delete(startPath);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Problem with unzipping the directory.");
+            }
+        }
+
+        private void deleteItems(string startPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(startPath);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (DirectoryInfo subdirectory in di.GetDirectories())
+            {
+                foreach (FileInfo file in subdirectory.GetFiles())
+                {
+                    file.Delete();
+                }
+                subdirectory.Delete();
+            }
+        }
+
+        #endregion
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                runTask.Abort();
+                deleteItems(selectedPath);
+                moveItems(backupPath, selectedPath);
+                foreach (var file in Directory.GetFiles(selectedPath))
+                {
+                    if (file.Contains("Zipped.zip"))
+                    {
+                        File.Delete(file);
+                    }
+                }
+                MessageBox.Show("Successfully canceled!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Oops! Something went wrong...");
+            }
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            pause = !pause;
+
+            if (pause) 
+            {
+                pauseButton.Text = "Continue";
+                mre.Reset();
+                textBox1.Text = "Process is stopped";
+            }
+            else
+            {
+                pauseButton.Text = "Pause";
+                mre.Set();
+                textBox1.Text = "Process is continued";
+
+            }
+        }
+    }
+}
+
+
